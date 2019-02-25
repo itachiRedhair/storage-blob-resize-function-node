cdc415a65c8e3b9cd7fdbda5dd732fe87771b56f
Akshay Milmile <akshay.milmile@gmail.com>
2019-02-25 12:20:19 +0530

    Update index.js

--- Thumbnail/index.js
+++ Thumbnail/index.js
@@ -1,27 +1,57 @@
 const stream = require('stream');
 const Jimp = require('jimp');
-
 const storage = require('azure-storage');
 const blobService = storage.createBlobService();
 
-module.exports = (context, myEvent, myBlob) => {
+const addBlobToAzureBlobStorage = (buffer, containerName, blobName) => {
+  const fileStream = new stream.Readable();
+  fileStream.push(buffer);
+  fileStream.push(null);
 
-  const widthInPixels = process.env.THUMBNAIL_WIDTH;
-  const blobName = myEvent.subject.split('/')[6];
+  return new Promise((resolve, reject) => {
+    blobService.createBlockBlobFromText(containerName, blobName, fileStream.read(), function(error, result, response) {
+      if (!error) {
+        context.log('[success] stored blob', blobName, 'in container', containerName);
+        resolve({ result, response });
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
 
-    Jimp.read(myBlob).then((thumbnail) => {
+module.exports = async function(context, eventGridEvent, blob) {
+  context.log(typeof eventGridEvent);
+  context.log(eventGridEvent);
 
-        thumbnail.resize(widthInPixels, Jimp.AUTO);
+  const smallThumbnailWidth = process.env.SMALL_THUMBNAIL_WIDTH;
+  const mediumThumbnailWidth = process.env.MEDIUM_THUMBNAIL_WIDTH;
+  const smallThumbnailContainer = process.env.SMALL_THUMBNAIL_CONTAINER_NAME;
+  const mediumThumbnailContainer = process.env.MEDIUM_THUMBNAIL_CONTAINER_NAME;
 
-        thumbnail.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+  const blobName = eventGridEvent.subject.split('/')[6];
 
-            const readStream = stream.PassThrough();
-            readStream.end(buffer);
+  Jimp.read(blob)
+    .then(jimpObject => {
+      const smallThumbnail = jimpObject.clone();
+      const mediumThumbnail = jimpObject.clon();
 
-            blobService.createBlockBlobFromStream('thumbnails', blobName, readStream, buffer.length, (err) => {
-                context.done();
-            });
-        });
+      smallthumbnail.resize(smallThumbnailWidth, Jimp.AUTO);
+      mediumThumbnail.resize(mediumThumbnailWidth, Jimp.AUTO);
 
-    }).catch(context.log);
+      const promise1 = smallThumbnail.getBufferAsync(Jimp.AUTO).then(buffer => {
+        context.log('[success] got buffer for small thumbnail', buffer);
+        return addBlobToAzureBlobStorage(buffer, smallThumbnailContainer, blobName);
+      });
+
+      const promise2 = mediumThumbnail.getBufferAsync(Jimp.AUTO).then(buffer => {
+        context.log('[success] got buffer for medium thumbnail', buffer);
+        return addBlobToAzureBlobStorage(buffer, mediumThumbnailContainer, blobName);
+      });
+
+      return Promise.all([promise1, promise2]);
+    })
+    .catch(err => {
+      context.log('[error]', err, 'for subject', eventGridEvent.subject);
+    });
 };
